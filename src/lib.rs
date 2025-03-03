81c8c7087e4d7800d304a557150b9882d1af94ee
p1k0chu <anatoliyid0207@gmail.com>
2025-03-03 14:21:56 +0200

    new function and unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
+use merge_json::Merge;
 use regex::{Captures, Regex};
 use serde_json::Value;
+use std::error::Error;
+use std::fs::{self, File};
+use std::path::Path;
+
+/// Parses all json files from the folder specified by `path` and merges them
+pub fn parse_and_merge(path: &Path) -> Result<Value, Box<dyn Error>> {
+    let mut result = Value::Null;
+
+    for file in fs::read_dir(path)? {
+        let file = file?.path();
+
+        if !file.extension().map_or(false, |x| x == "json") {
+            continue;
+        }
+
+        let reader = File::open(file)?;
+        let j: Value = serde_json::from_reader(&reader)?;
+
+        result.merge(&j);
+    }
+
+    Ok(result)
+}
 
 /// parses `string` and gets advancement ids from it, then replaces them with progresses from json
 ///
@@ -68,8 +92,11 @@ pub fn get_progress(json: &Value, adv_id: &str) -> Result<usize, ()> {
 
 #[cfg(test)]
 mod tests {
+    use std::fs::OpenOptions;
+
     use super::*;
     use serde_json::json;
+    use tempdir::TempDir;
 
     fn test_json() -> Value {
         json!({
@@ -96,5 +123,38 @@ mod tests {
         let output = String::from("Name 2/69");
 
         assert_eq!(&replace_with_progress(input, &test_json()), &output);
+    }
+
+    #[test]
+    fn parse_and_merge_test() -> Result<(), Box<dyn Error>> {
+        // name doesn't matter
+        let dir = TempDir::new("advancements")?;
+        let path = dir.path();
+
+        let j = json!({"advancement": {"criteria": {"first": 0}}});
+        serde_json::to_writer(
+            OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(path.join("file1.json"))?,
+            &j,
+        )?;
+
+        let j = json!({"advancement": {"criteria": {"second": 0}}});
+        serde_json::to_writer(
+            OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(path.join("file2.json"))?,
+            &j,
+        )?;
+
+        fs::write(path.join("non-json-file.txt"), "Hello world!")?;
+
+        let j = json!({"advancement": {"criteria": {"first":0, "second": 0}}});
+
+        assert_eq!(&(parse_and_merge(&path)?), &j);
+
+        Ok(())
     }
 }
